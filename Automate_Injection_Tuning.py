a1ea13d28bcef9397892d302e3c08f4b80921517
EhsanSoltan251 <97134888+EhsanSoltan251@users.noreply.github.com>
2023-07-18 14:40:35 -0600

    rework and add variation 2

--- Automate_Injection_Tuning.py
+++ Automate_Injection_Tuning.py
@@ -1,5 +1,7 @@
 from epics import caget, caput, cainfo, ca
 import time
+import math
+import sys
 
 stage_2_injection_efficiency_pv = "ICT1400-01:PCT1402-01:InjEff:fbk"
 
@@ -17,9 +19,8 @@ sth1_increment = 100000
 sth2_increment = 50000
 sth3_increment = 50000
 
-steering_magnets = [stv1, stv2, stv3, sth1, sth2, sth3]
-steering_magnet_increments = [stv1_increment, stv2_increment, stv3_increment,
-                              sth1_increment, sth2_increment, sth3_increment]
+magnet_to_increment = {stv1: stv1_increment, stv2: stv2_increment, stv3: stv3_increment,
+                       sth1: sth1_increment, sth2: sth2_increment, sth3: sth3_increment}
 
 latest_injection_efficiency = 0
 
@@ -51,55 +52,209 @@ def objectiveFunction():
     return latest_injection_efficiency
 
 
-def optimizeSteeringMagnet(pv_name, step, max_iterations):
-    
-    global latest_injection_efficiency
-
-    done = False
-    val = caget(pv_name)
-    iteration = 0
-    direction = -1
-
-    fitness = objectiveFunction()
-
-    while not done:
-
-        new_val = val + step * direction
-        caput(pv_name, new_val, wait=True)
-        new_fitness = objectiveFunction()
-
-        if new_fitness < fitness:
-            direction = direction * -1
-            caput(pv_name, val, wait=True)
-
-            #if it's not the first iteration and the injection efficiency got worse, then we know we stepped over the maximum
-            if iteration != 0:
-                return
-        else:
-            val = new_val
-            fitness = new_fitness
-        
-        iteration += 1
-        if iteration > max_iterations:
-            return
-    
-        print("Done tuning " + str(pv_name))
-
-
 '''
-    Tunes each magnet one by one
-    Params:
-        steering_magnet_pvs - a list containing the pv names of each steering magnet to tune in order
-        step_sizes - the step sizes to adjust each magnet (must be same order as steering_magnet_pvs)
+    This simple algorithm more or less follows the instructions on the wiki page for storage
+    ring injection tuning.
+    pv_name - the PV name of the steering magnet to tune
+    step - how large of a step to take for each adjustment
+    max_iterations - the maximum number of iterations before terminating the algorithm
 '''
-def performInjectionTuning(steering_magnet_pvs, step_sizes):
+def optimizeSteeringMagnetVariation1(pv_name, step, max_iterations):
 
     #subscribe to the injection efficiency PV. every time it changes we call the above OnChange() function
     injection_efficiency_channel = ca.create_channel(stage_2_injection_efficiency_pv)
     eventID = ca.create_subscription(injection_efficiency_channel, callback=onChange)
+    
+    global latest_injection_efficiency
 
-    for i in range(len(steering_magnet_pvs)):
-        optimizeSteeringMagnet(steering_magnet_pvs[i], step_sizes[i], 100)
+    done = False
 
-    print("Done tuning all steering magnets")
+    magnet_val = caget(pv_name)
+    initial_magnet_val = magnet_val
+
+    magnet_val_and_inj_eff = []
+
+
+    iteration = 0
+    direction = 1
+
+    injection_efficiency = objectiveFunction()
+    max_injection_efficiency = injection_efficiency
+
+    while not done:
+
+        magnet_val = magnet_val + step * direction
+        caput(pv_name, magnet_val, wait=True)
+
+        injection_efficiency = objectiveFunction()
+        if injection_efficiency > max_injection_efficiency:
+            max_injection_efficiency = injection_efficiency
+
+        if direction == 1:          # if we are going to the right
+            magnet_val_and_inj_eff.append((magnet_val, injection_efficiency))
+        elif direction == -1:       # if we are going to the left
+            magnet_val_and_inj_eff.insert(0, (magnet_val, injection_efficiency))
+
+        if injection_efficiency <= max_injection_efficiency * 0.8:
+            if direction == 1:
+                direction = -1
+                magnet_val = initial_magnet_val
+                caput(pv_name, initial_magnet_val, wait=True)
+            else:
+                done = True
+
+        iteration += 1
+        if iteration > max_iterations:      # just terminate the algorithm if we go past max iterations
+            print("Reached max iteration - terminating algorithm")
+            return
+        
+    
+    # here we clip the 'tail' so to speak so that we can find the approximate center of the flat top
+    done_clipping = False
+    worst_magnet = "left"
+    if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+        worst_magnet = "right"
+
+    while not done_clipping:
+
+        if worst_magnet == "left":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[1:]
+        elif worst_magnet == "right":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[0:len(magnet_val_and_inj_eff) - 1]
+
+        new_worst_magnet = "left"
+        if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+            new_worst_magnet = "right"
+        
+        if new_worst_magnet != worst_magnet:
+            done_clipping = True
+  
+
+    best_magnet_val = magnet_val_and_inj_eff[math.floor(len(magnet_val_and_inj_eff)/2)][0]
+    caput(pv_name, best_magnet_val, wait=True)
+        
+    
     ca.clear_subscription(injection_efficiency_channel) #unsubscribe from pv
+    print("Done tuning " + str(pv_name) + ", final magnet value: " + str(best_magnet_val))
+
+
+
+'''
+    This simple algorithm is the same as variation 1, with the caveat that we take the 
+    weighted mean center of the final 'strip chart' rather than just the center
+    pv_name - the PV name of the steering magnet to tune
+    step - how large of a step to take for each adjustment
+    max_iterations - the maximum number of iterations before terminating the algorithm
+'''
+def optimizeSteeringMagnetVariation2(pv_name, step, max_iterations):
+
+    #subscribe to the injection efficiency PV. every time it changes we call the above OnChange() function
+    injection_efficiency_channel = ca.create_channel(stage_2_injection_efficiency_pv)
+    eventID = ca.create_subscription(injection_efficiency_channel, callback=onChange)
+    
+    global latest_injection_efficiency
+
+    done = False
+
+    magnet_val = caget(pv_name)
+    initial_magnet_val = magnet_val
+
+    magnet_val_and_inj_eff = []
+
+
+    iteration = 0
+    direction = 1
+
+    injection_efficiency = objectiveFunction()
+    max_injection_efficiency = injection_efficiency
+
+    while not done:
+
+        magnet_val = magnet_val + step * direction
+        caput(pv_name, magnet_val, wait=True)
+
+        injection_efficiency = objectiveFunction()
+        if injection_efficiency > max_injection_efficiency:
+            max_injection_efficiency = injection_efficiency
+
+        if direction == 1:          # if we are going to the right
+            magnet_val_and_inj_eff.append([magnet_val, injection_efficiency])
+        elif direction == -1:       # if we are going to the left
+            magnet_val_and_inj_eff.insert(0, [magnet_val, injection_efficiency])
+
+        if injection_efficiency <= max_injection_efficiency * 0.8:
+            if direction == 1:
+                direction = -1
+                magnet_val = initial_magnet_val
+                caput(pv_name, initial_magnet_val, wait=True)
+            else:
+                done = True
+
+        iteration += 1
+        if iteration > max_iterations:      # just terminate the algorithm if we go past max iterations
+            print("Reached max iteration - terminating algorithm")
+            return
+        
+    
+    # here we clip the 'tail' so to speak so that we can find the approximate center of the flat top
+    done_clipping = False
+    worst_magnet = "left"
+    if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+        worst_magnet = "right"
+
+    while not done_clipping:
+
+        if worst_magnet == "left":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[1:]
+        elif worst_magnet == "right":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[0:len(magnet_val_and_inj_eff) - 1]
+
+        new_worst_magnet = "left"
+        if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+            new_worst_magnet = "right"
+        
+        if new_worst_magnet != worst_magnet:
+            done_clipping = True
+
+    leftmost_inj_eff = magnet_val_and_inj_eff[0][1]
+    rightmost_inj_eff = magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1]
+
+    if leftmost_inj_eff < rightmost_inj_eff:
+        magnet_val_and_inj_eff[0][1] += rightmost_inj_eff - leftmost_inj_eff
+    elif rightmost_inj_eff < leftmost_inj_eff:
+        magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] += leftmost_inj_eff - rightmost_inj_eff
+
+    
+    #find the weighted mean center
+    sum_weights = 0             # sum of all values (weights)
+    prod_xweights = 0           # product of all corresponding weights and positions
+    weighted_mean_center = 0    # weighted mean center
+
+    for i, val in enumerate(magnet_val_and_inj_eff):
+        sum_weights += val[1]
+        prod_xweights += (i + 1) * val[1]
+    
+    weighted_mean_center = int(prod_xweights / sum_weights - 1)
+    if weighted_mean_center < 0:
+        weighted_mean_center = 0
+    
+    best_magnet_val = magnet_val_and_inj_eff[weighted_mean_center][0]
+
+    caput(pv_name, best_magnet_val, wait=True)
+        
+    
+    ca.clear_subscription(injection_efficiency_channel) #unsubscribe from pv
+    print("Done tuning " + str(pv_name) + ", final magnet value: " + str(best_magnet_val))
+
+
+arg1 = str(sys.argv[1])
+arg2 = int(sys.argv[2])
+
+magnet_name = arg1.upper()
+if magnet_name[:-4] != ":adc":
+    magnet_name += ":adc"
+
+if arg2 == 1:
+    optimizeSteeringMagnetVariation1(magnet_name, magnet_to_increment[magnet_name], 100)
+if arg2 == 2:
+    optimizeSteeringMagnetVariation2(magnet_name, magnet_to_increment[magnet_name], 100)
--- Test_Injection_Tuning.py
+++ Test_Injection_Tuning.py
@@ -1,4 +1,7 @@
 import time
+import random
+import math
+import sys
 
 stage_2_injection_efficiency_pv = "ICT1400-01:PCT1402-01:InjEff:fbk"
 
@@ -17,10 +20,12 @@ sth2_increment = 50000
 sth3_increment = 50000
 
 steering_magnets = [stv1, stv2, stv3, sth1, sth2, sth3]
-steering_magnet_increments = [stv1_increment, stv2_increment, stv3_increment,
-                              sth1_increment, sth2_increment, sth3_increment]
+steering_magnet_increments = [stv1_increment, stv2_increment, stv3_increment, sth1_increment, sth2_increment, sth3_increment]
 
-pretend_steering_magnet_vals = [100000, 200000, 300000, 2000000, 300000, 600000]
+magnet_to_increment = {stv1: stv1_increment, stv2: stv2_increment, stv3: stv3_increment,
+                       sth1: sth1_increment, sth2: sth2_increment, sth3: sth3_increment}
+
+pretend_steering_magnet_vals = [1250000, 200000, 300000, 2000000, 300000, 600000]
 
 current_steering_magnet = stv1
 
@@ -42,67 +47,221 @@ def caput(pv, val, wait=True):
 
 
 ''' 
+    NOTE: JUST FOR TESTING STV1400-01
     This function determines the desirability of our output. Since we are just trying
     to maximize the injection efficiency, we just return it without modification
 '''
 def objectiveFunction():
 
-    total = 0
-    for val in pretend_steering_magnet_vals:
-        total += val / 100000
+    #center of flat top stv1-1400 = 1350000 with inj_eff = 97
+    #flat top extends 2 notches to left and right from the center
+    magnet_to_inj_eff = {1230000: 0, 1240000: 20, 1250000: 40, 1260000: 60, 1270000: 70, 1280000: 80, 1290000: 90,  
+                         1300000: 96, 1310000: 97, 1320000: 97, 1330000: 97, 1340000: 97, 1350000: 97, 1360000: 96, 
+                         1370000: 90, 1380000: 80, 1390000: 70, 1400000: 60, 1410000: 40, 1420000: 20, 1430000: 0  }
     
-    return total
+    return magnet_to_inj_eff[pretend_steering_magnet_vals[0]] + random.randint(0, 5)
 
 
-def optimizeSteeringMagnet(pv_name, step, max_iterations):
+
+def optimizeSteeringMagnetVariation1(pv_name, step, max_iterations):
+
+   
     
-    global current_steering_magnet
-    current_steering_magnet = pv_name
+    global latest_injection_efficiency
 
     done = False
-    val = caget(pv_name)
-    iteration = 0
-    direction = -1
 
-    fitness = objectiveFunction()
+    magnet_val = caget(pv_name)
+    initial_magnet_val = magnet_val
+
+    magnet_val_and_inj_eff = []
+
+
+    iteration = 0
+    direction = 1
+
+    injection_efficiency = objectiveFunction()
+    max_injection_efficiency = injection_efficiency
 
     while not done:
 
-        new_val = val + step * direction
-        caput(pv_name, new_val, wait=True)
-        new_fitness = objectiveFunction()
+        magnet_val = magnet_val + step * direction
+        caput(pv_name, magnet_val, wait=True)
 
-        if new_fitness < fitness:
-            direction = direction * -1
-            caput(pv_name, val, wait=True)
+        injection_efficiency = objectiveFunction()
+        if injection_efficiency > max_injection_efficiency:
+            max_injection_efficiency = injection_efficiency
 
-            #if it's not the first iteration and the injection efficiency got worse, then we know we stepped over the maximum
-            if iteration != 0:
-                print("Done tuning ", pv_name)
-                return
-        else:
-            val = new_val
-            fitness = new_fitness
+        if direction == 1:          # if we are going to the right
+            magnet_val_and_inj_eff.append((magnet_val, injection_efficiency))
+        elif direction == -1:       # if we are going to the left
+            magnet_val_and_inj_eff.insert(0, (magnet_val, injection_efficiency))
 
-        print("magnet val: ", val, " direction ", direction)
-        
+        if injection_efficiency <= max_injection_efficiency * 0.8:
+            if direction == 1:
+                direction = -1
+                magnet_val = initial_magnet_val
+                caput(pv_name, initial_magnet_val, wait=True)
+            else:
+                done = True
+
         iteration += 1
-        if iteration > max_iterations:
-            print("Done tuning ", pv_name)
+        if iteration > max_iterations:      # just terminate the algorithm if we go past max iterations
+            print("Reached max iteration - terminating algorithm")
             return
+        
     
+    done_clipping = False
+    worst_magnet = "left"
+    if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+        worst_magnet = "right"
+
+    while not done_clipping:
+
+        if worst_magnet == "left":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[1:]
+        elif worst_magnet == "right":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[0:len(magnet_val_and_inj_eff) - 1]
+
+        new_worst_magnet = "left"
+        if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+            new_worst_magnet = "right"
+        
+        if new_worst_magnet != worst_magnet:
+            done_clipping = True
+  
+
+    best_magnet_val = magnet_val_and_inj_eff[math.floor(len(magnet_val_and_inj_eff)/2)][0]
+    caput(pv_name, best_magnet_val, wait=True)
+        
+    
+  
+    print("Done tuning " + str(pv_name) + ", final magnet value: " + str(best_magnet_val))
+
+
 '''
-    Tunes each magnet one by one
-    Params:
-        steering_magnet_pvs - a list containing the pv names of each steering magnet to tune in order
-        step_sizes - the step sizes to adjust each magnet (must be same order as steering_magnet_pvs)
+    This simple algorithm is the same as variation 1, with the caveat that we take the 
+    weighted mean center of the final 'strip chart' rather than just the center
+    pv_name - the PV name of the steering magnet to tune
+    step - how large of a step to take for each adjustment
+    max_iterations - the maximum number of iterations before terminating the algorithm
 '''
-def performInjectionTuning(steering_magnet_pvs, step_sizes):
+def optimizeSteeringMagnetVariation2(pv_name, step, max_iterations):
 
-    for i in range(len(steering_magnet_pvs)):
-        optimizeSteeringMagnet(steering_magnet_pvs[i], step_sizes[i], 10)
-        print("Pretend injection efficiency: ", objectiveFunction())
+    done = False
 
-    print("Done tuning all steering magnets")
+    magnet_val = caget(pv_name)
+    initial_magnet_val = magnet_val
 
-performInjectionTuning(steering_magnets, steering_magnet_increments)
+    magnet_val_and_inj_eff = []
+
+
+    iteration = 0
+    direction = 1
+
+    injection_efficiency = objectiveFunction()
+    max_injection_efficiency = injection_efficiency
+
+    while not done:
+
+        magnet_val = magnet_val + step * direction
+        caput(pv_name, magnet_val, wait=True)
+
+        injection_efficiency = objectiveFunction()
+        if injection_efficiency > max_injection_efficiency:
+            max_injection_efficiency = injection_efficiency
+
+        if direction == 1:          # if we are going to the right
+            magnet_val_and_inj_eff.append([magnet_val, injection_efficiency])
+        elif direction == -1:       # if we are going to the left
+            magnet_val_and_inj_eff.insert(0, [magnet_val, injection_efficiency])
+
+        if injection_efficiency <= max_injection_efficiency * 0.8:
+            if direction == 1:
+                direction = -1
+                magnet_val = initial_magnet_val
+                caput(pv_name, initial_magnet_val, wait=True)
+            else:
+                done = True
+
+        iteration += 1
+        if iteration > max_iterations:      # just terminate the algorithm if we go past max iterations
+            print("Reached max iteration - terminating algorithm")
+            return
+        
+    
+    # here we clip the 'tail' so to speak so that we can find the approximate center of the flat top
+    done_clipping = False
+    worst_magnet = "left"
+    if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+        worst_magnet = "right"
+
+    while not done_clipping:
+
+        if worst_magnet == "left":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[1:]
+        elif worst_magnet == "right":
+            magnet_val_and_inj_eff = magnet_val_and_inj_eff[0:len(magnet_val_and_inj_eff) - 1]
+
+        new_worst_magnet = "left"
+        if magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] < magnet_val_and_inj_eff[0][1]:
+            new_worst_magnet = "right"
+        
+        if new_worst_magnet != worst_magnet:
+            done_clipping = True
+
+
+
+    #here we equalize the leftmost and rightmost injection efficiencies
+    leftmost_inj_eff = magnet_val_and_inj_eff[0][1]
+    rightmost_inj_eff = magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1]
+
+    if leftmost_inj_eff < rightmost_inj_eff:
+        magnet_val_and_inj_eff[0][1] = rightmost_inj_eff
+    elif rightmost_inj_eff < leftmost_inj_eff:
+        magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1][1] = leftmost_inj_eff
+
+    
+    #duplicate the edges before applying moving average
+    magnet_val_and_inj_eff.insert(0, magnet_val_and_inj_eff[0])
+    magnet_val_and_inj_eff.append(magnet_val_and_inj_eff[len(magnet_val_and_inj_eff)-1])
+
+    #apply moving average to smooth out noise
+    for i in range(1, len(magnet_val_and_inj_eff) - 2):
+        
+        avg = (magnet_val_and_inj_eff[i-1][1] + magnet_val_and_inj_eff[i][1] + magnet_val_and_inj_eff[i+1][1])/3
+        magnet_val_and_inj_eff[i][1] = avg
+
+    #find the weighted mean center
+    sum_weights = 0             # sum of all values (weights)
+    prod_xweights = 0           # product of all corresponding weights and positions
+    weighted_mean_center = 0    # weighted mean center
+
+    for i, val in enumerate(magnet_val_and_inj_eff):
+        sum_weights += val[1]
+        prod_xweights += (i + 1) * val[1]
+    
+    weighted_mean_center = int(prod_xweights / sum_weights - 1)
+    if weighted_mean_center < 0:
+        weighted_mean_center = 0
+    
+    best_magnet_val = magnet_val_and_inj_eff[weighted_mean_center][0]
+
+    caput(pv_name, best_magnet_val, wait=True)
+        
+    
+    print("Done tuning " + str(pv_name) + ", final magnet value: " + str(best_magnet_val))
+
+
+    
+arg1 = str(sys.argv[1])
+arg2 = int(sys.argv[2])
+
+magnet_name = arg1.upper()
+if magnet_name[:-4] != ":adc":
+    magnet_name += ":adc"
+
+if arg2 == 1:
+    optimizeSteeringMagnetVariation1(magnet_name, magnet_to_increment[magnet_name], 100)
+if arg2 == 2:
+    optimizeSteeringMagnetVariation2(magnet_name, magnet_to_increment[magnet_name], 100)
